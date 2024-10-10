e5484d09318131fe111c390a95d478565df158fd
Bruna Calistrate <bruna_klistrat@hotmail.com>
2024-10-09 23:09:05 -0300

    fix: organize datasets, remove timeout condition

--- dags/clever_main_DAG.py
+++ dags/clever_main_DAG.py
@@ -1,40 +1,41 @@
-from datetime import datetime, timedelta
+from datetime import datetime
 from airflow import DAG
 from airflow.operators.empty import EmptyOperator
 from airflow.operators.python import PythonOperator
+from scripts.clever_main_pipeline import upload_to_postgres
 
 default_args = {
-    "owner": "alec.ventura",
+    "owner": "bruna.calistrate",
     "start_date": datetime(2024, 10, 1),
 }
 
 datasets = [
-    'fmcsa_complaints.csv',
-    'fmcsa_safer_data.csv',
-    'fmcsa_company_snapshot.csv',
-    'fmcsa_companies.csv',
-    'customer_reviews_google.csv',
-    'company_profiles_google_maps.csv'
-
+    "company_profiles_google_maps.csv",
+    "customer_reviews_google.csv",
+    "fmcsa_companies.csv",
+    "fmcsa_company_snapshot.csv",
+    "fmcsa_safer_data.csv",
 ]
 
-with DAG("clever_main_DAG", default_args=default_args, catchup=False, schedule_interval='20 0 * * *', max_active_runs=1) as dag:
-
-    start_task = EmptyOperator(task_id='Start', dag=dag)
-    finish_task = EmptyOperator(task_id='Finish', dag=dag)
+with DAG(
+    "clever_main_DAG",
+    default_args=default_args,
+    catchup=False,
+    schedule_interval="20 0 * * *",
+    max_active_runs=1,
+) as dag:
+    start_task = EmptyOperator(task_id="Start", dag=dag)
+    finish_task = EmptyOperator(task_id="Finish", dag=dag)
 
     for file in datasets:
-        file_without_extension = file.split('.')[0]
+        file_without_extension = file.split(".")[0]
 
         task_id = f"upload_to_postgres_{file_without_extension}"
         upload_to_postgres_task = PythonOperator(
             task_id=task_id,
             python_callable=upload_to_postgres,
             dag=dag,
-            execution_timeout=timedelta(milliseconds=2),
-            op_kwargs={
-                "file_name": file
-            }
+            op_kwargs={"file_name": file},
         )
 
         start_task.set_downstream(upload_to_postgres_task)
